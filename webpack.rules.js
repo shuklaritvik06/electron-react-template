59fe6924725097c0d823b35ef891eb70bdb94b1f
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-03-07 22:37:26 +0530

    init

---
+++ webpack.rules.js
@@ -0,0 +1,39 @@
+module.exports = [
+  {
+    test: /native_modules[/\\].+\.node$/,
+    use: "node-loader"
+  },
+  {
+    test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
+    parser: { amd: false },
+    use: {
+      loader: "@vercel/webpack-asset-relocator-loader",
+      options: {
+        outputAssetBase: "native_modules"
+      }
+    }
+  },
+  {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader"
+    }
+  }
+  // Put your webpack loader rules in this array.  This is where you would put
+  // your ts-loader configuration for instance:
+  /**
+   * Typescript Example:
+   *
+   * {
+   *   test: /\.tsx?$/,
+   *   exclude: /(node_modules|.webpack)/,
+   *   loaders: [{
+   *     loader: 'ts-loader',
+   *     options: {
+   *       transpileOnly: true
+   *     }
+   *   }]
+   * }
+   */
+];
